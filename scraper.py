52eb2137ab6ab7746838cf4a2b57f2733c5a03ce
Korkorka <jakub.revak@gmail.com>
2024-01-31 22:10:52 +0100

    done with the scraper for today

--- scraper.py
+++ scraper.py
@@ -21,14 +21,23 @@ def scrape_site(url_input):
     soup = BeautifulSoup(page, "html.parser")
     content = soup.find("div", class_="entry-content")
     content_divs = content.find_all("div", class_="wp-block-image")
+    content_divs2 = content.find_all("figure", class_="wp-block-image size-large")
+    content_info = content.find_all("h3", class_="wp-block-heading has-text-align-left")
     for div in content_divs:
-            under_div = div.find("img")
-            img_src = under_div.get("src")
-            img_list.append(img_src.encode("utf-8"))
-            print(img_src.encode("utf-8"))
+        under_div = div.find("img")
+        img_src = under_div.get("src")
+        img_list.append(img_src.encode("utf-8"))
+    for fig in content_divs2:
+        under_fig = fig.find("img")
+        img_src = under_fig.get("src")
+        img_list.append(img_src.encode("utf-8"))
+    for inf in content_info:
+        under_inf = inf.find("strong")
+        info = under_inf.get_text().encode("utf-8")
+        info_list.append(info)
     print(img_list)
     print(info_list)
-    return 0
+    return img_list
 
 scrape_site(ucitelia)
         
